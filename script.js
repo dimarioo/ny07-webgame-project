0ab116c0a996d8ec4fe201e4080d4a072460dfe0
Dimario Oliver <Dimario Oliver>
2023-02-18 10:42:40 -0500

    Pulled Question from API and Load On Page

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+
 async function getquestions(){
  let response = await fetch('https://the-trivia-api.com/api/questions/')
- let question = await response.json()
- console.log(question)
+ let APIdata = await response.json()
+    return APIdata;
+
 }
 
-getquestions()
+
+getquestions().then((APIdata) => {
+    console.log(APIdata)
+    document.getElementById('category').innerHTML = APIdata[0].category;
+    document.getElementById('question').innerHTML = APIdata[0].question;
+
+})
