8380c2b20a718e01f39ede01488611c31f90b328
Dimario Oliver <Dimario Oliver>
2023-02-25 12:31:04 -0500

    Added ability for answers arr to shuffle

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-
+//Fetch data from triva API 
 async function getquestions(){
- let response = await fetch('https://the-trivia-api.com/api/questions/')
+ let response = await fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple')
  let APIdata = await response.json()
-    return APIdata;
-
+return APIdata;
 }
 
+function shufflearray(arry){
+    console.log(arry)
+    for (let i = arry.length - 1; i >= 0; i--){
+        const s = Math.floor(Math.random() * (i + 1));
+        [arry[i], arry[s]] = [arry[s] , arry[i]];
+        console.log(i)
+        console.log(s)
+        console.log(arry)
+    }
+} 
 
+
+
+//Display Question to User
 getquestions().then((APIdata) => {
-    console.log(APIdata)
-    document.getElementById('category').innerHTML = APIdata[0].category;
-    document.getElementById('question').innerHTML = APIdata[0].question;
+    const results = APIdata.results[0];
+    console.log(results)
+    document.getElementById('category').innerHTML = results.category;
+    document.getElementById('question').innerHTML = results.question;
+    const answers = [...results.incorrect_answers, results.correct_answer]
+    shufflearray(answers)
 
-})
+    document.getElementById('button').innerHTML = answers[0]
+    document.getElementById('button1').innerHTML = answers[1]
+    document.getElementById('button2').innerHTML = answers[2]
+    document.getElementById('button3').innerHTML = answers[3]
+
+})   
+
+
+    
